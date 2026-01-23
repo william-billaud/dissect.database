7eb2cdca1457bc5261a4a4db55476a5b227d288b
wbi <william.billaud@orange.com>
2026-01-23 15:04:35 +0100

    issue exchange attributes on 2012 R2

---
+++ dissect/database/ese/tools/ntds.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import argparse
+import json
+from pathlib import Path
+
+from dissect.database.ese.ntds import NTDS
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="dissect.database.ese NTDS parser")
+    parser.add_argument("input", help="NTDS database to read")
+    parser.add_argument("-o", "--objectClass", help="show only 'object'", required=True)
+    parser.add_argument("-j", "--json", help="output in JSON format", action="store_true", default=False)
+    args = parser.parse_args()
+
+    with Path(args.input).open("rb") as fh:
+        ntds = NTDS(fh)
+
+        for record in ntds.search(objectClass=args.objectClass):
+            if args.json:
+                print(json.dumps(record, default=str))
+            else:
+                print(record)
+
+
+if __name__ == "__main__":
+    main()
